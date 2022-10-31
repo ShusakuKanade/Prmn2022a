c697615517b4747f8801f9be877e5af0ea606aef
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-10-31 17:29:29 +0900

    課題1-4 完了

--- prmn2022a/prmn2022a/src/lecture01/Main.java
+++ prmn2022a/prmn2022a/src/lecture01/Main.java
@@ -10,12 +10,25 @@ public class Main {
         //System.out.println("B" + studentNumber + " Minato Hatanaka");
 
         //課題1-3
-        int age = 19;
+        //int age = 19;
 
-        if(age < 20){
-            System.out.println("I am " + age + " years old so I cannot drink liquor.");
-        }else if(age >= 20){
-            System.out.println("I am " + age + " years old so I can drink liquior.");
+        //if(age < 20){
+        //    System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        //}else if(age >= 20){
+        //    System.out.println("I am " + age + " years old so I can drink liquior.");
+        //}
+
+        //課題1-4
+        int[] array = new int[100];
+        int sum = 0;
+
+        for(int i = 0;i < array.length;i++){//forにした理由:条件式でiを1ずつ増やす処理ができるから
+            array[i] = i + 1;
+            if(i % 2 == 0){
+                sum = sum + array[i];
+            }
         }
+
+        System.out.println(sum);
     }
 }
