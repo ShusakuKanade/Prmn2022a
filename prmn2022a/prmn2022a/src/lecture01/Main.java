7b0935881a712dd62c134565904f819c7301d37b
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-10-31 17:13:49 +0900

    課題1-2 完了

--- prmn2022a/prmn2022a/src/lecture01/Main.java
+++ prmn2022a/prmn2022a/src/lecture01/Main.java
@@ -1,2 +1,12 @@
-package lecture01;public class Main {
+package lecture01;
+
+public class Main {
+    public static void main(String[] args) {
+        //課題1-1
+        //System.out.println("B2211890 Minato Hatanaka");
+
+        //課題1-2
+        int studentNumber = 2211890;
+        System.out.println("B" + studentNumber + " Minato Hatanaka");
+    }
 }
