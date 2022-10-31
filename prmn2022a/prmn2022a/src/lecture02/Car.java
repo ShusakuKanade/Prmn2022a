950568a1e43602e31cd428587925a464edce81e4
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-10-31 22:31:08 +0900

    課題2-3　完了

--- prmn2022a/prmn2022a/src/lecture02/Car.java
+++ prmn2022a/prmn2022a/src/lecture02/Car.java
@@ -2,9 +2,13 @@ package lecture02;
 
 public class Car {
     int fuel;
+    Tire[] tires = new Tire[4];
+    Engine engine;
 
-    Car(){
+    Car(Tire[] tires,Engine engine){
         this.fuel = 0;
+        this.tires = tires;
+        this.engine = engine;
     }
 
     void run(){
@@ -15,5 +19,9 @@ public class Car {
 
         System.out.println("燃料を1消費して走りました。");
         fuel = fuel - 1;
+    }
+
+    void startEngine(){
+        engine.start();
     }
 }
