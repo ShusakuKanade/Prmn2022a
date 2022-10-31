a69721341bbb7adddb2950ee3808177832f06a54
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-10-31 17:18:48 +0900

    課題1-3 完了

--- prmn2022a/prmn2022a/src/lecture01/Main.java
+++ prmn2022a/prmn2022a/src/lecture01/Main.java
@@ -6,7 +6,16 @@ public class Main {
         //System.out.println("B2211890 Minato Hatanaka");
 
         //課題1-2
-        int studentNumber = 2211890;
-        System.out.println("B" + studentNumber + " Minato Hatanaka");
+        //int studentNumber = 2211890;
+        //System.out.println("B" + studentNumber + " Minato Hatanaka");
+
+        //課題1-3
+        int age = 19;
+
+        if(age < 20){
+            System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        }else if(age >= 20){
+            System.out.println("I am " + age + " years old so I can drink liquior.");
+        }
     }
 }
