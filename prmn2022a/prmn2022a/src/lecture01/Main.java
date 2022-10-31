80a3b7fb3c3f57ac14527549c7b8cb566dc01f1f
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-10-31 18:02:48 +0900

    課題1 完了

--- prmn2022a/prmn2022a/src/lecture01/Main.java
+++ prmn2022a/prmn2022a/src/lecture01/Main.java
@@ -3,33 +3,33 @@ package lecture01;
 public class Main {
     public static void main(String[] args) {
         //課題1-1
-        //System.out.println("B2211890 Minato Hatanaka");
+        System.out.println("B2211890 Minato Hatanaka");
 
         //課題1-2
-        //int studentNumber = 2211890;
-        //System.out.println("B" + studentNumber + " Minato Hatanaka");
+        int studentNumber = 2211890;
+        System.out.println("B" + studentNumber + " Minato Hatanaka");
 
         //課題1-3
-        //int age = 19;
+        int age = 19;
 
-        //if(age < 20){
-        //    System.out.println("I am " + age + " years old so I cannot drink liquor.");
-        //}else if(age >= 20){
-        //    System.out.println("I am " + age + " years old so I can drink liquior.");
-        //}
+        if(age < 20){
+            System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        }else if(age >= 20){
+            System.out.println("I am " + age + " years old so I can drink liquior.");
+        }
 
         //課題1-4
-        //int[] array = new int[100];
-        //int sum = 0;
+        int[] array = new int[100];
+        int sum = 0;
 
-        //for(int i = 0;i < array.length;i++){//forにした理由:条件式でiを1ずつ増やす処理ができるから
-        //    array[i] = i + 1;
-        //    if(i % 2 == 0){
-        //        sum = sum + array[i];
-        //    }
-        //}
+        for(int i = 0;i < array.length;i++){//forにした理由:条件式でiを1ずつ増やす処理ができるから
+            array[i] = i + 1;
+            if(i % 2 == 0){
+                sum = sum + array[i];
+            }
+        }
 
-        //System.out.println(sum);
+        System.out.println(sum);
 
         //課題1-5
         int[] score = {41,85,72,38,80};
