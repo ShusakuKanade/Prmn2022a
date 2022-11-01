e5a49a8a230d472a0ef0509c6aa552e641609e32
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-11-01 21:08:15 +0900

    課題3-2 修正

--- prmn2022a/prmn2022a/src/lecture03/Exercise3_2.java
+++ prmn2022a/prmn2022a/src/lecture03/Exercise3_2.java
@@ -10,15 +10,16 @@ public class Exercise3_2 {
 
         System.out.println("何行分入力しますか？");
         int times = input.nextInt();
+        String blank = input.nextLine();
 
         for(int i=0;i<times;i++){
             System.out.println(i+1 + "行目:");
-            strings.add(input.next());
+            strings.add(input.nextLine());
         }
 
         System.out.println("入力した文字列:");
-        for(int i=0;i<times;i++){
-            System.out.println("[" + i + "]" + strings.get(i));
+        for(String string : strings){
+            System.out.println(string);
         }
     }
 }
