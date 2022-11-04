f71479d4e00e305c4df00751d9c423708c5c01e5
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-11-04 19:39:53 +0900

    課題6-2 完了

---
+++ prmn2022a/prmn2022a/src/lecture06/Exercise6_2.java
@@ -0,0 +1,30 @@
+package lecture06;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercise6_2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        //System.out.println(random.nextInt(100));
+
+        for(int i=0;i<5;i++){
+            list.add(random.nextInt(6));
+        }
+        System.out.println("さいころを5つ振りました。");
+
+        System.out.println("何番目のさいころの値を確認しますか？");
+        try{
+            int x = input.nextInt();
+            System.out.println(x + "番目のさいころの値は" + list.get(x));
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("AllayListの範囲外アクセスを確認しました。");
+            System.out.println("プログラムを終了します。");
+        }
+    }
+}
