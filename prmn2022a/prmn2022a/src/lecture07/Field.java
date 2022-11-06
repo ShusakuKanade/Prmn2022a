055157f4f60047bb313d5a241f5a7d829daccc7e
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-11-06 17:40:56 +0900

    フィールド 作成

---
+++ prmn2022a/prmn2022a/src/lecture07/Field.java
@@ -0,0 +1,55 @@
+package lecture07;
+
+public class Field implements IBattleMonster{
+    private IBattleMonster pLayerMonster;
+    private IBattleMonster enemyMonster;
+
+    public Field(PlayerMonster pLayerMonster,EnemyMonster enemyMonster){
+
+    }
+
+    private void battle(){
+
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public int skillSelect() {
+        return 0;
+    }
+
+    @Override
+    public void attackOutput() {
+
+    }
+
+    @Override
+    public void remainHpOutput() {
+
+    }
+
+    @Override
+    public void skillListOutPut() {
+
+    }
+
+    @Override
+    public void winOutput() {
+
+    }
+
+    @Override
+    public void loseOutput() {
+
+    }
+
+
+    //private boolean Turn(IBattleMonster first,IBattleMonster second){}
+
+    //private int commandSelectPhase(IBattleMonster command){}
+
+}
