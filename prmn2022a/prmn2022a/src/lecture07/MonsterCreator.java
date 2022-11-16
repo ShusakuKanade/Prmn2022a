d76b0c67607c465c44506f623c57264f10ba0b40
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-11-16 17:49:19 +0900

    MonsterCreator 更新

--- prmn2022a/prmn2022a/src/lecture07/MonsterCreator.java
+++ prmn2022a/prmn2022a/src/lecture07/MonsterCreator.java
@@ -1,19 +1,26 @@
 package lecture07;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
 public enum MonsterCreator {
-    MONKEY,
-    PENGUIN,
-    TURTLE;
+    MONKEY(0),
+    PENGUIN(1),
+    TURTLE(2);
 
     private int id;
     private Supplier<Monster> supplier;
-    private static Map<Integer,MonsterCreator> ;
+    private static Map<Integer,MonsterCreator> map = new HashMap<Integer,MonsterCreator>();
+
+    private MonsterCreator(int id){
+        this.id = id;
+    }
 
     public void viewMonsterList(){
-
+        for(MonsterCreator monsters:MonsterCreator.values()){
+            System.out.println(monsters.id + "->" + monsters);
+        }
     }
 
     public int monsterMinId(){
