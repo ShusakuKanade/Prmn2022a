f27395f96f27d579b420e37d5eccd91d4e21c981
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-11-16 17:33:29 +0900

    Main 更新

--- prmn2022a/prmn2022a/src/lecture07/Exercise7_1.java
+++ prmn2022a/prmn2022a/src/lecture07/Exercise7_1.java
@@ -2,9 +2,23 @@ package lecture07;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Exercise7_1 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        
+
+    }
+
+    private static int stdinMonsterId(){
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
+    }
+
+    private static int randomMonsterId(){
+        Random rand = new Random();
+        return rand.nextInt(3);
     }
 }
