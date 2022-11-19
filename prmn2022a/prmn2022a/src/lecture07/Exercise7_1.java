2a985f3cda82293c6aa8e2f049282be9d6632d43
[ShusakuKanade] <[minato0111@outlook.jp]>
2022-11-19 20:56:19 +0900

    Main 更新

--- prmn2022a/prmn2022a/src/lecture07/Exercise7_1.java
+++ prmn2022a/prmn2022a/src/lecture07/Exercise7_1.java
@@ -1,16 +1,19 @@
 package lecture07;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Exercise7_1 {
 
     public static void main(String[] args) {
-        
+        MonsterCreator.viewMonsterList();
+        System.out.print("モンスターを選べ:");
+        stdinMonsterId();
+        randomMonsterId();
 
     }
+
+
 
     private static int stdinMonsterId(){
         Scanner input = new Scanner(System.in);
--- prmn2022a/prmn2022a/src/lecture07/MonsterCreator.java
+++ prmn2022a/prmn2022a/src/lecture07/MonsterCreator.java
@@ -4,31 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
-public enum MonsterCreator {
+public enum MonsterCreator{
     MONKEY(0),
     PENGUIN(1),
     TURTLE(2);
 
     private int id;
     private Supplier<Monster> supplier;
-    private static Map<Integer,MonsterCreator> map = new HashMap<Integer,MonsterCreator>();
+    private static Map<Integer, lecture07.MonsterCreator> map = new HashMap<Integer, lecture07.MonsterCreator>();
 
-    private MonsterCreator(int id){
-        this.id = id;
-    }
-
-    public void viewMonsterList(){
-        for(MonsterCreator monsters:MonsterCreator.values()){
+    MonsterCreator(int id){this.id = id;}
+    public static void viewMonsterList(){
+        for(lecture07.MonsterCreator monsters: lecture07.MonsterCreator.values()){
             System.out.println(monsters.id + "->" + monsters);
         }
     }
 
-    public int monsterMinId(){
+    //public static int monsterMinId(){}
 
-    }
-
-    public int monsterMaxId(){
-
-    }
+    //public static int monsterMaxId(){}
 
 }
+
